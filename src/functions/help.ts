f31df99baada0266bb175385f6ceec21f0964b9b
Pekka Lammi <pekkalammi@Pekkas-MacBook-Pro.local>
2020-03-22 17:09:21 +0200

    clarification

--- src/functions/help.ts
+++ src/functions/help.ts
@@ -2,12 +2,11 @@ import { authenticateUser } from "..";
 import { ContextMessageUpdate } from "telegraf";
 
 const help = (ctx: ContextMessageUpdate) => {
-  console.log(ctx);
   const from = ctx.update.message.from;
   if (authenticateUser(from.id)) {
     return ctx.reply(
       "Hei sinä. Olen kyklooppi ja tehtävänäni on varmistaa Wapovallan siisteys ja puhtaus. Olet päässyt jo pitkälle. Olet jo täällä. Apollo rakastaa sinua. Nyt sinun aikasi on rakastaa Apolloa takaisin. \n\nSeuraava taulukko näyttää sinulle rakkaudentunnustuksestasi oikeuttavat pisteet. \n\n" +
-        "/apk tyhjäys & leijuvat likaiset sisään 5p \n" +
+        "/apk puhtaiden tyhjäys & leijuvat likaiset sisään 5p \n" +
         "/bio roskis-run sisältäen bion 5p \n" +
         "/roskat roskis-run ilman bioo 5p \n" +
         "/pullot kauppaan 5p \n" +
