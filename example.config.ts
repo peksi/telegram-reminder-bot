e571e54e08ee5d696d35f5cd3f22e1f16ee1ed38
Pekka Lammi <pekkalammi@Pekkas-MacBook-Pro.local>
2020-03-22 16:01:30 +0200

    update config to es6

--- example.config.ts
+++ example.config.ts
@@ -1,12 +1,13 @@
-module.exports = {
-    telegraf_token:'<TOKEN_HERE>',
-    users: [ // telegram userID's that are permitted to use commands
-        123,
-        456,
-        789
-      ],
-    broadcastChatId: -123456789, // chat where you want the reminders to be sent. use /info command to help you with this
-    dbPath: "~/db.json" // path where .json database is saved
-
+const config = {
+  telegraf_token: "<TOKEN_HERE>",
+  users: [
+    // telegram userID's that are permitted to use commands
+    123,
+    456,
+    789
+  ],
+  broadcastChatId: -123456789, // chat where you want the reminders to be sent. use /info command to help you with this
+  dbPath: "~/db.json" // path where .json database is saved
 };
 
+export default config;
